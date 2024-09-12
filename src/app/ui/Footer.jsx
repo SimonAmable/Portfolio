599ff0ba4d7837b890be5d65d360e8af5032586d
SimonAmable <125688417+SimonAmable@users.noreply.github.com>
2024-09-12 00:52:13 -0400

    fixed bugs, discord icon and contact

--- src/app/ui/Footer.jsx
+++ src/app/ui/Footer.jsx
@@ -6,11 +6,14 @@ import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 const Footer = () => {
   return (
-    <div className='flex gap-6 text-white bg-black w-full justify-center items-center py-10 '>
-        <Link href="https://github.com/SimonAmable" ><Image className='hover:opacity-70' src='/github.svg' width={40} height={40} alt='github'/></Link>
-        <Link href="https://www.linkedin.com/in/simon-amable-59ab091ab/" ><Image src='/linkedin.svg' width={50} height={50} alt='linkin'/></Link>
-        <Link href="" ><Image src='/discord-icon.svg' width={40} height={40} alt='linkin'/></Link>
-        <Link href="mailto:simonamable@gmail.com" ><MailOutlineIcon className='w-10 h-10'/></Link>
+    <div className='flex flex-col items-center bg-black py-10'>
+      <p className='text-white text-lg py-2'>Contact me:</p>
+      <div id="footer"className='flex gap-6 text-white bg-black w-full justify-center items-center  '>
+          <Link href="https://github.com/SimonAmable" ><Image className='hover:opacity-70' src='/github.svg' width={40} height={40} alt='github'/></Link>
+          <Link href="https://www.linkedin.com/in/simon-amable-59ab091ab/" ><Image src='/linkedin.svg' width={50} height={50} alt='linkin'/></Link>
+          {/* <Link href="" ><Image src='/discord-icon.svg' width={40} height={40} alt='discord'/></Link> */}
+          <Link href="mailto:simonamable@gmail.com" ><MailOutlineIcon className='w-10 h-10'/></Link>
+      </div>
     </div>
   )
 }
