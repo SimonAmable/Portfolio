2a00f5c428c478670256ea9f7fd899e702b941fd
SimonAmable <125688417+SimonAmable@users.noreply.github.com>
2024-09-13 19:02:13 -0400

    bug fixes

--- src/app/ui/Footer.jsx
+++ src/app/ui/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
           <Link href="https://github.com/SimonAmable" ><Image className='hover:opacity-70' src='/github.svg' width={40} height={40} alt='github'/></Link>
           <Link href="https://www.linkedin.com/in/simon-amable-59ab091ab/" ><Image src='/linkedin.svg' width={50} height={50} alt='linkin'/></Link>
           {/* <Link href="" ><Image src='/discord-icon.svg' width={40} height={40} alt='discord'/></Link> */}
-          <Link href="mailto:simonamable@gmail.com" ><MailOutlineIcon className='w-10 h-10'/></Link>
+          <Link href="mailto:simonamable@gmail.com" ><MailOutlineIcon className='w-10 h-10 '/></Link>
       </div>
     </div>
   )
